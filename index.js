2e1ecd1061276484a27f73752505180735c7ae84
Rafael <rafaeldubois974@gmail.com>
2021-09-28 13:16:22 +0400

    ca fonctionne ?

---
+++ index.js
@@ -0,0 +1,9 @@
+const userInfo = require("./information")
+console.log(userInfo)
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "I'm a moooodule",
+    e : "oO",
+    T : "U "
+}));
---
+++ information.js
@@ -0,0 +1,9 @@
+const objet = {
+  name: "Dubois Rafael",
+  campus: "Strasbourg",
+}
+module.exports = {
+    name: objet.name,
+    campus: objet.campus,
+
+}
